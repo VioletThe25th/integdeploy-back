5d1a6d42eb400a9e38b0a5ca437d96614b6ad822
Jeremy Bilger <bilgerjeremy5705@gmail.com>
2025-01-17 10:02:14 +0100

    Update the user model

--- model/user.js
+++ model/user.js
@@ -5,6 +5,11 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     firstName: String,
     lastName: String,
+    email: { type: String, required: true },
+    postalCode: String,
+    city: String,
+    birthday: Date,
 });
+
 
 module.exports = mongoose.model("User", UserSchema);
